ef4e9c0852c7e68c2d47cbd9d38f2a62ac95d5cd
Schumova Maria <schumova.maria@gmail.com>
2023-08-14 11:13:29 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+
+  Array.from(str).map(
+    (symbol) => 
+      bracketsConfig.map((sequence) => {
+        if (sequence.includes(symbol)) {
+          sequence[0] === symbol && sequence[0] !== sequence[1] ||
+          stack[stack.length - 1] !== sequence[0] ?
+          stack.push(symbol) :
+          stack.pop()
+        }
+      }
+    )
+  )
+
+  return stack.length ? false : true
 }
